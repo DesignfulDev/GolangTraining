74b3d994c8f09efe31a925392e3d5222d73654f7
mauricelacerda <lacerda@protonmail.com>
2017-08-07 22:55:53 -0300

    playing with formats and numeral systems

---
+++ 02_numeralSystems/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Printf("%d \t %b \t %#x \t %q \n", 42, 42, 42, 42)
+	fmt.Printf("%d \t %b \t %#X \t %q \n", 42, 42, 42, 42)
+	fmt.Println("########################################")
+
+	for i := 1000000; i < 1000100; i++ {
+		fmt.Printf("%d \t %b \t %U \t %#x \t %#o \t %q \n", i, i, i, i, i, i)
+	}
+}
